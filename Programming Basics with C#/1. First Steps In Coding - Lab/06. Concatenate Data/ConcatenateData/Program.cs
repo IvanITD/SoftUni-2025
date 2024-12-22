2038d4d53f70660f15f484e1b4907d1b218aa2a4
Ivan Ivanov <ivan78608@gmail.com>
2024-12-22 18:15:02 +0200

    Concatenate Data Solved

---
+++ Programming Basics with C#/1. First Steps In Coding - Lab/06. Concatenate Data/ConcatenateData/Program.cs
@@ -0,0 +1,8 @@
+﻿//The inputs
+string firstName = Console.ReadLine();
+string lastName = Console.ReadLine();
+string age = Console.ReadLine();
+string town = Console.ReadLine();
+
+//The output
+Console.WriteLine($"You are {firstName} {lastName}, a {age}-years old person from {town}.");
