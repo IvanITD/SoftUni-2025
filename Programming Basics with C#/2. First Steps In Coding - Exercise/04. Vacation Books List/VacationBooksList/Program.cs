61d931b1938a6b902eb5d9a54303f52996137c77
Ivan Ivanov <ivan78608@gmail.com>
2024-12-23 01:51:51 +0200

    Vacation Books List Solved

---
+++ Programming Basics with C#/2. First Steps In Coding - Exercise/04. Vacation Books List/VacationBooksList/Program.cs
@@ -0,0 +1,13 @@
+﻿//User Inputs
+int pagesAmount = int.Parse(Console.ReadLine());
+int pagesPerHour = int.Parse(Console.ReadLine());
+int days = int.Parse(Console.ReadLine());
+
+//Total time to read the book
+int totalTime = pagesAmount / pagesPerHour;
+
+//Needed hours per day
+int neededHoursPerDay = totalTime / days;
+
+//Output
+Console.WriteLine(neededHoursPerDay);
