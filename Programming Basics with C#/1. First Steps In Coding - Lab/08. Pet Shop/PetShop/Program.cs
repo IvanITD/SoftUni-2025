8474ca18ef3b919a6c2082427f0dfc1cf72c82dc
Ivan Ivanov <ivan78608@gmail.com>
2024-12-23 00:32:52 +0200

    Pet Shop Solved

---
+++ Programming Basics with C#/1. First Steps In Coding - Lab/08. Pet Shop/PetShop/Program.cs
@@ -0,0 +1,16 @@
+﻿int dogFoodAmount = int.Parse(Console.ReadLine());
+int catFoodAmount = int.Parse(Console.ReadLine());
+
+//Food Price
+double dogFoodPrice = 2.50;
+int catFoodPrice = 4;
+
+//Dog and Cat Food Total
+double dogFoodTotal = dogFoodAmount * dogFoodPrice;
+int catFoodTotal = catFoodAmount * catFoodPrice;
+
+//Final Price
+double finalPrice = dogFoodTotal + catFoodTotal;
+
+//The output
+Console.WriteLine($"{finalPrice} lv.");
