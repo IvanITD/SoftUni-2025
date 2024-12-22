9f248a443a73bd72acf8272c685af721c008930b
Ivan Ivanov <ivan78608@gmail.com>
2024-12-23 01:41:42 +0200

    Deposit Calculator Solved

---
+++ Programming Basics with C#/2. First Steps In Coding - Exercise/03. Deposit Calculator/DepositCalculator/Program.cs
@@ -0,0 +1,10 @@
+﻿//User inputs
+double depositSum = double.Parse(Console.ReadLine());
+int depositPeriod = int.Parse(Console.ReadLine());
+double yearPercent = double.Parse(Console.ReadLine());
+
+//Calculations
+double TotalSum = depositSum + depositPeriod * ((depositSum * yearPercent / 100) / 12);
+
+//Output
+Console.WriteLine(TotalSum);
