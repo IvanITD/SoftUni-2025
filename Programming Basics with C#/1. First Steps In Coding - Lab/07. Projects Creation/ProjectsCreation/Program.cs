bd4a7cbc3bb531531d70d366dd509f79a4928c7e
Ivan Ivanov <ivan78608@gmail.com>
2024-12-23 00:24:08 +0200

    Project Creation Solved

---
+++ Programming Basics with C#/1. First Steps In Coding - Lab/07. Projects Creation/ProjectsCreation/Program.cs
@@ -0,0 +1,10 @@
+﻿//Receiving the input from the user
+string name = Console.ReadLine();
+int projectAmount = int.Parse(Console.ReadLine());
+
+//Calculating the needed time
+int neededTime = projectAmount * 3;
+
+//Printing the result
+Console.WriteLine($"The architect {name} will need {neededTime} hours to complete {projectAmount} project/s.");
+
