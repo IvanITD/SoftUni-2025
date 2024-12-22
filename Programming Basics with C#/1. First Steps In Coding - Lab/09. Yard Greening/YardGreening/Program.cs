1f3eda245f39c1b906cad633e332b2ce208b131c
Ivan Ivanov <ivan78608@gmail.com>
2024-12-23 01:09:40 +0200

    Yard Greening Solved

---
+++ Programming Basics with C#/1. First Steps In Coding - Lab/09. Yard Greening/YardGreening/Program.cs
@@ -0,0 +1,18 @@
+﻿//User input
+double greeningArea = double.Parse(Console.ReadLine());
+
+//Greening Price per square meter
+double greeningPrice = 7.61;
+
+//The Total Price
+double price = greeningArea * greeningPrice;
+
+//The discount
+double discount = price * 0.18;
+
+//Total price after discount
+double totalPrice = price - discount;
+
+//The output
+Console.WriteLine($"The final price is: {totalPrice} lv.");
+Console.WriteLine($"The discount is: {discount}");
