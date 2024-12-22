9c432c8f5e826c21e142a63af3943450b5f228fa
Ivan Ivanov <ivan78608@gmail.com>
2024-12-23 01:31:04 +0200

    Radians to Degrees Solved

---
+++ Programming Basics with C#/2. First Steps In Coding - Exercise/02. Radians to Degrees/RadiansToDegrees/Program.cs
@@ -0,0 +1,8 @@
+﻿//User input
+double radians = double.Parse(Console.ReadLine());
+
+//Turning radians to degrees
+double degrees = radians * 180 / Math.PI;
+
+//Output
+Console.WriteLine($"{degrees}");
