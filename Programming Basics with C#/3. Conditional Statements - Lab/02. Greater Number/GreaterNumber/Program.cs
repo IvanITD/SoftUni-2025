391976e87c7bbe9e4123b8356063a13a154952c4
Ivan Ivanov <ivan78608@gmail.com>
2024-12-23 14:43:06 +0200

    Greater Number Solved

---
+++ Programming Basics with C#/3. Conditional Statements - Lab/02. Greater Number/GreaterNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿//User Inputs
+int firstNumber = int.Parse(Console.ReadLine());
+int secondNumber = int.Parse(Console.ReadLine());
+
+if (firstNumber > secondNumber)
+{
+    Console.WriteLine(firstNumber);
+}
+else
+{
+    Console.WriteLine(secondNumber);
+}
