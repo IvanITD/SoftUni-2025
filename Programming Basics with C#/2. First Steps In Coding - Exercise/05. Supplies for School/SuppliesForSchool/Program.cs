a6c2470fcda8aa653be63800983fcf6478defb82
Ivan Ivanov <ivan78608@gmail.com>
2024-12-23 02:10:43 +0200

    Supplies for School Solved

---
+++ Programming Basics with C#/2. First Steps In Coding - Exercise/05. Supplies for School/SuppliesForSchool/Program.cs
@@ -0,0 +1,27 @@
+﻿//User inputs
+int penAmount = int.Parse(Console.ReadLine());
+int markerAmount = int.Parse(Console.ReadLine());
+int detergentLiters = int.Parse(Console.ReadLine());
+int discountPercentage = int.Parse(Console.ReadLine());
+
+//Price for each product
+double penPrice = 5.80;
+double markerPrice = 7.20;
+double detergentPrice = 1.20;
+
+//Calculations
+double penTotalPrice = penAmount * penPrice;
+double markerTotalPrice = markerAmount * markerPrice;
+double detergentTotalPrice = detergentLiters * detergentPrice;
+
+//The total price
+double totalPrice = penTotalPrice + markerTotalPrice + detergentTotalPrice;
+
+//Discount
+double discount = totalPrice * discountPercentage / 100;
+
+//Price with the discount
+double discountedPrice = totalPrice - discount;
+
+//The output
+Console.WriteLine($"{discountedPrice}");
