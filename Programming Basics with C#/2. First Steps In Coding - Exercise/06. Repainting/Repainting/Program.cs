7dc80318e1930e40bba75e77c38daf3195be44ec
Ivan Ivanov <ivan78608@gmail.com>
2024-12-23 03:01:54 +0200

    Working on Repainting

---
+++ Programming Basics with C#/2. First Steps In Coding - Exercise/06. Repainting/Repainting/Program.cs
@@ -0,0 +1,33 @@
+﻿//User inputs
+int nylonAmount = int.Parse(Console.ReadLine());
+int paintLiters = int.Parse(Console.ReadLine());
+int thinnerLiters = int.Parse(Console.ReadLine());
+int hours = int.Parse(Console.ReadLine());
+
+//Price for each product
+double nylonPrice = 1.50;
+double paintPrice = 14.50;
+double thinnerPrice = 5.00;
+
+//More materials needed
+double moreNylon = nylonAmount + 2;
+double morePaint = paintLiters * 0.10;
+double bagPrice = 0.40;
+
+//Calculations
+double nylonTotalPrice = (nylonAmount + moreNylon) * nylonPrice; // + 2 for more nylon
+double paintTotalPrice = (paintLiters * morePaint) * paintPrice;
+double thinnerTotalPrice = thinnerLiters * thinnerPrice;
+
+//Total Price
+double sumPrice = nylonTotalPrice + paintTotalPrice + thinnerTotalPrice;
+
+//money for workers
+double workersPrice = sumPrice * 0.30;
+double workersTotalPrice = hours * workersPrice;
+
+//TotalPrice
+double totalPrice = sumPrice + workersTotalPrice + bagPrice;
+
+//Output
+Console.WriteLine(totalPrice);
