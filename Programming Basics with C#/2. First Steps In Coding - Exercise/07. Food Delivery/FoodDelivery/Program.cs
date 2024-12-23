9e303e0db9e3d5b9334fdaf44ebe1870d3049f20
Ivan Ivanov <ivan78608@gmail.com>
2024-12-23 12:44:43 +0200

    Finished Food Delivery and started the Basketball Equipment

---
+++ Programming Basics with C#/2. First Steps In Coding - Exercise/07. Food Delivery/FoodDelivery/Program.cs
@@ -0,0 +1,29 @@
+﻿//User Inputs
+int chickenMenu = int.Parse(Console.ReadLine());
+int fishMenu = int.Parse(Console.ReadLine());
+int veganMenu = int.Parse(Console.ReadLine());
+
+//Price of each menu
+double chickenMenuPrice = 10.35;
+double fishMenuPrice = 12.40;
+double veganMenuPrice = 8.15;
+
+//Summing of each menu separately
+double chickenMenuTotal = chickenMenu * chickenMenuPrice;
+double fishMenuTotal = fishMenu * fishMenuPrice;
+double veganMenuTotal = veganMenu * veganMenuPrice;
+
+//Combining the total price of all menus
+double totalMenuPrice = chickenMenuTotal + fishMenuTotal + veganMenuTotal;
+
+//Desert Price = 20% of the total price
+double desertPrice = totalMenuPrice * 0.20;
+
+//Delivery Price
+double deliveryPrice = 2.50;
+
+//Total Price
+double totalPrice = totalMenuPrice + desertPrice + deliveryPrice;
+
+//Total Price Output
+Console.WriteLine(totalPrice);
