db7da149915881f788578ab8dfdd207b33b7f958
Ivan Ivanov <ivan78608@gmail.com>
2024-12-23 17:24:17 +0200

    Area of Figures Solved

---
+++ Programming Basics with C#/3. Conditional Statements - Lab/07. Area of Figures/AreaOfFigures/Program.cs
@@ -0,0 +1,35 @@
+﻿string viewFigure = Console.ReadLine();
+
+if (viewFigure == "square")
+{
+    double squareLength = double.Parse(Console.ReadLine());
+    double squareArea = squareLength * squareLength;
+
+    Console.WriteLine($"{squareArea:f3}");
+}
+else if (viewFigure == "rectangle")
+{
+    double rectangleSide1 = double.Parse(Console.ReadLine());
+    double rectangleSide2 = double.Parse(Console.ReadLine());
+
+    double rectangleArea = rectangleSide1 * rectangleSide2;
+
+    Console.WriteLine($"{rectangleArea:f3}");
+
+}
+else if (viewFigure == "circle")
+{
+    double circleRadius = double.Parse(Console.ReadLine());
+    double circleArea = circleRadius * circleRadius * Math.PI;
+
+    Console.WriteLine($"{circleArea:f3}");
+}
+else if (viewFigure == "triangle")
+{
+    double triangleLength = double.Parse(Console.ReadLine());
+    double triangleHeight = double.Parse(Console.ReadLine());
+
+    double triangleArea = (triangleLength * triangleHeight) / 2;
+
+    Console.WriteLine($"{triangleArea:f3}");
+}
