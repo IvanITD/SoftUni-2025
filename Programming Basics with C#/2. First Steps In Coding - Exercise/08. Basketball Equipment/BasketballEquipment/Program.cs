06711176a7f36869adc2bc08fd19bccd373aa1d9
Ivan Ivanov <ivan78608@gmail.com>
2024-12-23 13:07:24 +0200

    Basketball Equipment Solved

--- Programming Basics with C#/2. First Steps In Coding - Exercise/08. Basketball Equipment/BasketballEquipment/Program.cs
+++ Programming Basics with C#/2. First Steps In Coding - Exercise/08. Basketball Equipment/BasketballEquipment/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//User Inputs
+int yearlyFee = int.Parse(Console.ReadLine());
+
+//Price of each equipment
+double basketballShoes = yearlyFee - (yearlyFee * 0.40); // 40% of the yearly fee
+double basketballClothes = basketballShoes - (basketballShoes * 0.20); // 20% cheaper than the shoes
+double basketBall = basketballClothes / 4; // 1/4 of the basketball clothes
+double basketballAccessories = basketBall / 5; // 1/5 of the basket ball
+
+//Total Price
+double totalPrice = yearlyFee + basketballShoes + basketballClothes + basketBall + basketballAccessories;
+
+//Output
+Console.WriteLine(totalPrice);
