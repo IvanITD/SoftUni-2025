4e44c40b043cae7b84f8f8f14c2f9f8f86b87112
Ivan Ivanov <ivan78608@gmail.com>
2024-12-23 13:54:48 +0200

    Fish Tank Resolved

---
+++ Programming Basics with C#/2. First Steps In Coding - Exercise/09. Fish Tank/FishTank/Program.cs
@@ -0,0 +1,20 @@
+﻿//User Inputs
+int length = int.Parse(Console.ReadLine());
+int width = int.Parse(Console.ReadLine());
+int height = int.Parse(Console.ReadLine());
+double usedPercentOfArea = double.Parse(Console.ReadLine());
+
+//Calculations of each area
+double volume = length * width * height;
+
+// 1 Liter = 0.001 m3
+double volumeInLiters = volume * 0.001;
+
+//Calculations of used space
+double usedSpace = usedPercentOfArea / 100;
+
+//Needed liters of water
+double neededLiters = volumeInLiters * (1 - usedSpace);
+
+//Output
+Console.WriteLine(neededLiters);
