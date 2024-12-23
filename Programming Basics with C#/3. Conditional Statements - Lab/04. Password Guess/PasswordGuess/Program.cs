98ed44d5ce430bdaa65f908aa5980469c1ff6b9c
Ivan Ivanov <ivan78608@gmail.com>
2024-12-23 15:58:13 +0200

    Password Guess Solved

---
+++ Programming Basics with C#/3. Conditional Statements - Lab/04. Password Guess/PasswordGuess/Program.cs
@@ -0,0 +1,12 @@
+﻿//User input
+string password = Console.ReadLine();
+
+//Check if the password is correct
+if (password == "s3cr3t!P@ssw0rd")
+{
+    Console.WriteLine("Welcome");
+}
+else
+{
+    Console.WriteLine("Wrong password!");
+}
