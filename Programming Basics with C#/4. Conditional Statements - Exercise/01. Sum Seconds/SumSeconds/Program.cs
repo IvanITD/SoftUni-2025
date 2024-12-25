bf6c5c5c33a8048b6740c5b1a0c30997bcdbce5c
Ivan Ivanov <ivan78608@gmail.com>
2024-12-25 21:13:25 +0200

    Bonus Score Solved

--- Programming Basics with C#/4. Conditional Statements - Exercise/01. Sum Seconds/SumSeconds/Program.cs
+++ Programming Basics with C#/4. Conditional Statements - Exercise/01. Sum Seconds/SumSeconds/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿//User Input
+int firstTime = int.Parse(Console.ReadLine());
+int secondTime = int.Parse(Console.ReadLine());
+int thirdTime = int.Parse(Console.ReadLine());
+
+//Sum of the times
+int totalTime = firstTime + secondTime + thirdTime;
+
+//Turn the total time into minutes and seconds
+double minutes = totalTime / 60;
+double seconds = totalTime % 60;
+
+//Check if the seconds are less than 10 in order to add a 0 in front of them
+if (seconds < 10)
+{
+    //Print the output
+    Console.WriteLine($"{minutes}:0{seconds}");
+}
+else
+{
+    //Print the output
+    Console.WriteLine($"{minutes}:{seconds}");
+}
---
+++ Programming Basics with C#/4. Conditional Statements - Exercise/02. Bonus Score/BonusScore/Program.cs
@@ -0,0 +1,57 @@
+﻿//User Inputs
+int number = int.Parse(Console.ReadLine());
+
+double bonusPoints = 0.0;
+//Check on how much are the points
+if (number <= 100)
+{
+    bonusPoints = 5; //5 points
+
+    //More bonus points
+    if (number % 2 == 0)
+    {
+        bonusPoints += 1; //+1 point
+    }
+    else if (number % 10 == 5)
+    {
+        bonusPoints += 2; //+2 points
+    }
+
+}
+else if (number > 1000)
+{
+    bonusPoints = number * 0.1; //10% of the number
+
+    //More bonus points
+    if (number % 2 == 0)
+    {
+        bonusPoints += 1; //+1 point
+    }
+    else if (number % 10 == 5)
+    {
+        bonusPoints += 2; //+2 points
+    }
+}
+else if (number > 100 || number <= 1000)
+{
+    bonusPoints = number * 0.2; //20% of the number
+
+    //More bonus points
+    if (number % 2 == 0)
+    {
+        bonusPoints += 1; //+1 point
+    }
+    else if (number % 10 == 5)
+    {
+        bonusPoints += 2; //+2 points
+    }
+}
+
+
+
+//Sum the number with the points
+double sum = number + bonusPoints;
+
+//Print the output
+Console.WriteLine(bonusPoints); // Print the bonus points
+Console.WriteLine(sum); // Print the total sum
