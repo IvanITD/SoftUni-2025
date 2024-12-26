5c7805860bacb2dda25c51f88b939d83740444e7
Ivan Ivanov <ivan78608@gmail.com>
2024-12-26 18:11:17 +0200

    Godzilla vs. Kong Solved

---
+++ Programming Basics with C#/4. Conditional Statements - Exercise/05. Godzilla vs. Kong/GodzillaVSKong/Program.cs
@@ -0,0 +1,35 @@
+﻿//User Inputs
+double filmBudget = double.Parse(Console.ReadLine());
+int statistsAmount = int.Parse(Console.ReadLine());
+double clothPricePerStatist = double.Parse(Console.ReadLine());
+
+
+//Prices and calculations
+double decorPrice = filmBudget * 0.10;
+double clothPrice = statistsAmount * clothPricePerStatist;
+
+//Check if statists amount is bigger than 150 so that we can apply discount on the clothe price
+if (statistsAmount >= 150)
+{
+    clothPrice = clothPrice - (clothPrice * 0.10); // 10% discount
+    
+    
+
+    
+}
+
+//Total Price for clothes and decor
+    double totalPrice = decorPrice + clothPrice;
+
+
+//Logically check if we have enough money for the film
+    if (totalPrice > filmBudget)
+    {
+        Console.WriteLine("Not enough money!");
+        Console.WriteLine($"Wingard needs {totalPrice - filmBudget:f2} leva more.");
+    }
+    else
+    {
+        Console.WriteLine("Action!");
+        Console.WriteLine($"Wingard starts filming with {filmBudget - totalPrice:f2} leva left.");
+    }
