47f37ff3ac1105cff141e5ac7a09228f27a9a592
Ivan Ivanov <ivan78608@gmail.com>
2024-12-27 19:28:49 +0200

    Personal Titles Switch Statement Solved

---
+++ Programming Basics with C#/5. Conditional Statements Advanced - Lab/04. Personal Titles/PersonalTitles/Program.cs
@@ -0,0 +1,33 @@
+﻿//User Inputs
+double age = double.Parse(Console.ReadLine());
+char gender = char.Parse(Console.ReadLine());
+
+//Check if the user is a male, man or a boy, and a female, girl or a woman
+switch (gender)
+{
+    case 'm':
+    switch (age)
+    {
+        case >=16:
+        Console.WriteLine("Mr.");
+        break;
+        
+        case <16:
+        Console.WriteLine("Master");
+        break;
+    }
+    break;
+
+    case 'f':
+    switch (age)
+    {
+        case >=16:
+        Console.WriteLine("Ms.");
+        break;
+
+        case <16:
+        Console.WriteLine("Miss");
+        break;
+    }
+    break;
+}
