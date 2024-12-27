de23564246f35c728f6f2b5fbb181ad2f6ef1d12
Ivan Ivanov <ivan78608@gmail.com>
2024-12-27 02:44:25 +0200

    Lunch Break Solved

---
+++ Programming Basics with C#/4. Conditional Statements - Exercise/08. Lunch Break/LunchBreak/Program.cs
@@ -0,0 +1,23 @@
+﻿//User Input
+string movieName = Console.ReadLine();
+int episodeDuration = int.Parse(Console.ReadLine());
+int breakDuration = int.Parse(Console.ReadLine());
+
+//Calculating lunch and relaxation time
+double lunchTime = breakDuration / 8.0;
+double relaxTime = breakDuration / 4.0;
+
+//Calculating how much free time is left.
+double freeTime = breakDuration - lunchTime - relaxTime;
+
+//Checking if the time is enough to watch the movie.
+if(freeTime >= episodeDuration)
+{
+    //Output
+    Console.WriteLine($"You have enough time to watch {movieName} and left with {Math.Ceiling(freeTime - episodeDuration)} minutes free time.");
+}
+else
+{
+    //Output
+    Console.WriteLine($"You don't have enough time to watch {movieName}, you need {Math.Ceiling(episodeDuration - freeTime)} more minutes.");
+}
