c0362fe65e03ede5bbf04e2e2d6c6a6853e8a673
Ivan Ivanov <ivan78608@gmail.com>
2024-12-27 17:20:14 +0200

    Animal Type If-Else Statement

---
+++ Programming Basics with C#/5. Conditional Statements Advanced - Lab/03. Animal Type/AnimalTypeIfElse/Program.cs
@@ -0,0 +1,14 @@
+﻿string animalType = Console.ReadLine();
+
+if (animalType == "dog")
+{
+    Console.WriteLine("mammal");
+}
+else if (animalType == "crocodile" || animalType == "tortoise" || animalType == "snake")
+{
+    Console.WriteLine("reptile");
+}
+else
+{
+    Console.WriteLine("unknown");
+}
