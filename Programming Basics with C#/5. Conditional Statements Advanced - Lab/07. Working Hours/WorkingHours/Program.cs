a2c27a7d15480a0287aba5b9a86049861772f88c
Ivan Ivanov <ivan78608@gmail.com>
2024-12-29 00:22:24 +0200

    Working Hours Solved in If-Else statement, because switch statements cannot use arithmetic operators or complex boolean expressions directly within switch case

---
+++ Programming Basics with C#/5. Conditional Statements Advanced - Lab/07. Working Hours/WorkingHours/Program.cs
@@ -0,0 +1,30 @@
+﻿//User Input
+int dayTime = int.Parse(Console.ReadLine());
+string weekDay = Console.ReadLine();
+
+//Check if dayTime is in working hours
+switch (weekDay)
+{
+    case "Monday":
+    case "Tuesday":
+    case "Wednesday":
+    case "Thursday":
+    case "Friday":
+    case "Saturday":
+        if (dayTime >= 10 && dayTime <= 18)
+        {
+            Console.WriteLine("open");
+        }
+        else
+        {
+            Console.WriteLine("closed");
+        }
+        break;
+
+    case "Sunday":
+
+        {
+            Console.WriteLine("closed");
+        }
+        break;
+}
