998c62c5cd34097e54ecc834cce99d88af7d9c18
Ivan Ivanov <ivan78608@gmail.com>
2024-12-29 02:34:50 +0200

    Invalid Number Solved

---
+++ Programming Basics with C#/5. Conditional Statements Advanced - Lab/10. Invalid Number/InvalidNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿// User Input
+int num = int.Parse(Console.ReadLine());
+
+//Check if the number is in between 100 and 200 or equal to 0
+if (num >= 100 && num <= 200 || num == 0)
+{
+    Console.WriteLine();
+}
+else
+{
+    Console.WriteLine("invalid");
+}
