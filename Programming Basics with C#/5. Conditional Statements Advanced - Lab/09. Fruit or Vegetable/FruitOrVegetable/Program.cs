10d33072b2b8d6661546755ce8d4b8e7d5995dcb
Ivan Ivanov <ivan78608@gmail.com>
2024-12-29 02:01:56 +0200

    Fruit or Vegetable Switch and If-Else Statement Solved

--- Programming Basics with C#/5. Conditional Statements Advanced - Lab/09. Fruit or Vegetable/FruitOrVegetable/Program.cs
+++ Programming Basics with C#/5. Conditional Statements Advanced - Lab/09. Fruit or Vegetable/FruitOrVegetable/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿//User Input
+string product = Console.ReadLine();
+
+//Check if product is fruit
+switch (product)
+{
+    case "banana":
+    case "apple":
+    case "kiwi":
+    case "cherry":
+    case "lemon":
+    case "grapes":
+        Console.WriteLine("fruit");
+        break;
+
+    case "tomato":
+    case "cucumber":
+    case "pepper":
+    case "carrot":
+        Console.WriteLine("vegetable");
+        break;
+
+    default:
+        Console.WriteLine("unknown");
+        break;
+}
---
+++ Programming Basics with C#/5. Conditional Statements Advanced - Lab/09. Fruit or Vegetable/FruitOrVegetableIfElse/Program.cs
@@ -0,0 +1,18 @@
+﻿//User Input
+string product = Console.ReadLine();
+
+//Check if product is fruit
+if(product == "banana" || product == "apple" || product == "kiwi" || product == "cherry" || product == "lemon" || product == "grapes")
+{
+    Console.WriteLine("fruit");
+}
+//Check if product is vegetable
+else if (product == "tomato" || product == "cucumber" || product == "pepper" || product == "carrot")
+{
+    Console.WriteLine("vegetable");
+}
+//If product is not fruit or vegetable
+else
+{
+    Console.WriteLine("unknown");
+}
