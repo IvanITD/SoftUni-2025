0c0d2d0b684f7c00a42768f061db0d24d95a25f1
Ivan Ivanov <ivan78608@gmail.com>
2024-12-29 17:50:07 +0200

    Cinema Switch & If-Else Statement Solved

---
+++ Programming Basics with C#/6. Conditional Statements Advanced - Exercise/01. Cinema/Cinema/Program.cs
@@ -0,0 +1,27 @@
+﻿string projectionType = Console.ReadLine();
+int rows = int.Parse(Console.ReadLine());
+int columns = int.Parse(Console.ReadLine());
+
+double projectionPrice = 0.0;
+double totalPrice = 0.0;
+
+switch (projectionType)
+{
+    case "Premiere":
+    projectionPrice = 12.00;
+    totalPrice = rows * columns * projectionPrice;
+    break;
+
+    case "Normal":
+    projectionPrice = 7.50;
+    totalPrice = rows * columns * projectionPrice;
+    break;
+
+    case "Discount":
+    projectionPrice = 5.00;
+    totalPrice = rows * columns * projectionPrice;
+    break;
+
+}
+
+Console.WriteLine($"{totalPrice:f2} leva");
---
+++ Programming Basics with C#/6. Conditional Statements Advanced - Exercise/01. Cinema/CinemaIfElse/Program.cs
@@ -0,0 +1,24 @@
+﻿string projectionType = Console.ReadLine();
+int rows = int.Parse(Console.ReadLine());
+int columns = int.Parse(Console.ReadLine());
+
+double projectionPrice = 0.0;
+double totalPrice = 0.0;
+
+if (projectionType == "Premiere")
+{
+    projectionPrice = 12.00;
+    totalPrice = rows * columns * projectionPrice;
+}
+else if (projectionType == "Normal")
+{
+    projectionPrice = 7.50;
+    totalPrice = rows * columns * projectionPrice;
+}
+else if (projectionType == "Discount")
+{
+    projectionPrice = 5.00;
+    totalPrice = rows * columns * projectionPrice;
+}
+
+Console.WriteLine($"{totalPrice:f2} leva");
