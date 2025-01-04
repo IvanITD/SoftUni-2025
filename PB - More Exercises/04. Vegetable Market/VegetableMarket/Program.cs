5fe154e80937b2cb1af26959cb373d9b8bc694b7
Ivan Ivanov <ivan78608@gmail.com>
2025-01-04 14:30:17 +0200

    Vegetable Market from PB More Exercises Solved

---
+++ PB - More Exercises/04. Vegetable Market/VegetableMarket/Program.cs
@@ -0,0 +1,11 @@
+﻿double vegetablePrice = double.Parse(Console.ReadLine());
+double fruitPrice = double.Parse(Console.ReadLine());
+int vegetableKg = int.Parse(Console.ReadLine());
+int fruitKg = int.Parse(Console.ReadLine());
+
+double vegetableTotal = vegetableKg * vegetablePrice;
+double fruitTotal = fruitKg * fruitPrice;
+
+double total = (vegetableTotal + fruitTotal) / 1.94;
+
+Console.WriteLine($"{total:f2}");
