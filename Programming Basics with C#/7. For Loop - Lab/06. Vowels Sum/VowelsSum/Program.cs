933307fb3613f5875087f86c0b3a6b5213200c32
Ivan Ivanov <ivan78608@gmail.com>
2025-01-05 13:40:17 +0200

    Vowels Sum Solved

---
+++ Programming Basics with C#/7. For Loop - Lab/06. Vowels Sum/VowelsSum/Program.cs
@@ -0,0 +1,36 @@
+﻿//User Input
+string text = Console.ReadLine();
+
+//Variable for later use
+int sum = 0;
+
+//Check to see if the letter is a vowel and if yes add the value to the sum
+for (int i = 0; i < text.Length; i++)
+{
+    char letter = text[i];
+
+    switch (letter)
+    {
+        case 'a':
+            sum += 1;
+            break;
+
+        case 'e':
+            sum += 2;
+            break;
+
+        case 'i':
+            sum += 3;
+            break;
+
+        case 'o':
+            sum += 4;
+            break;
+
+        case 'u':
+            sum += 5;
+            break;
+    }
+}
+
+Console.WriteLine(sum);
