ff4acb13f5949af7b7990c183ee54ac8bb6c6f12
Ivan Ivanov <ivan78608@gmail.com>
2025-01-05 15:06:39 +0200

    Sum Numbers Solved

---
+++ Programming Basics with C#/7. For Loop - Lab/07. Sum Numbers/SumNumbers/Program.cs
@@ -0,0 +1,10 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+double total = 0;
+
+for (int i = 0; i < n; i++)
+{
+    double number = double.Parse(Console.ReadLine());
+    total += number;
+}
+Console.WriteLine(total);
