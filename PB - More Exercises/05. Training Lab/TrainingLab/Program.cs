108ff61d03012e584bc3e201e2111c7a6844524d
Ivan Ivanov <ivan78608@gmail.com>
2025-01-09 23:00:02 +0200

    Finished Training Lab in PB More Exercises

--- PB - More Exercises/05. Training Lab/TrainingLab/Program.cs
+++ PB - More Exercises/05. Training Lab/TrainingLab/Program.cs
@@ -1,3 +1,23 @@
-﻿double w = double.Parse(Console.ReadLine());
+﻿//User input
+double w = double.Parse(Console.ReadLine());
 double h = double.Parse(Console.ReadLine());
 
+//Turning meters to centimeters
+double widthCm = w * 100;
+double heightCm = h * 100;
+
+// Now I need to remove the 100 cm for the corridor
+heightCm = heightCm - 100;
+
+// Amount of desks in a row (by width) and amount of rows (by height)
+int desksPerRow = (int)heightCm / 70;
+int rows = (int)widthCm / 120;
+
+// Total amount of desks
+int totalDesks = desksPerRow * rows;
+
+// Removing three desks for the teacher's desk and the front door
+totalDesks -= 3;
+
+//Printing the result
+ Console.WriteLine(totalDesks);
