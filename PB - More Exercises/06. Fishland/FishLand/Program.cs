7735f695f260e47521a14b0d74fe13f643ff914a
Ivan Ivanov <ivan78608@gmail.com>
2025-01-10 02:47:48 +0200

    FishLand Solved

---
+++ PB - More Exercises/06. Fishland/FishLand/Program.cs
@@ -0,0 +1,24 @@
+﻿// User Inputs
+double mackerelPricePerKg = double.Parse(Console.ReadLine()); // Цена на скумрията за килограм
+double spartPricePerKg = double.Parse(Console.ReadLine()); //  Цена на цацата за килограм
+double bonitoKg = double.Parse(Console.ReadLine()); // Количество паламуд
+double safridKg = double.Parse(Console.ReadLine()); // Количество сафрид
+int musselsKg = int.Parse(Console.ReadLine()); // Количество миди
+
+// Palamud Calculations and Price
+double bonitoPrice = mackerelPricePerKg + (mackerelPricePerKg * 0.60);
+double bonitoSum = bonitoKg * bonitoPrice;
+
+// Safrid Calculations and Price
+double safridPrice = spartPricePerKg + (spartPricePerKg * 0.80);
+double safridSum = safridKg * safridPrice;
+
+// Midi Calculations and Price
+double musselsPrice = 7.50;
+double musselsSum = musselsKg * musselsPrice;
+
+// Total Sum
+double totalSum = bonitoSum + safridSum + musselsSum;
+
+// Output
+Console.WriteLine($"{totalSum:f2}");
