21a12309741502a925d01b560cceca64a3daaefc
Ivan Ivanov <ivan78608@gmail.com>
2025-01-10 23:43:54 +0200

    Circle Area and Perimeter Solved

---
+++ PB - More Exercises/08. Circle Area and Perimeter/CircleAreaAndPerimeter/Program.cs
@@ -0,0 +1,11 @@
+﻿double r = double.Parse(Console.ReadLine());
+
+// Area of a circle
+double areaOfCircle = r * r * Math.PI;
+
+// Perimeter of a circle
+double perimeterOfCircle = 2 * Math.PI * r;
+
+// Print the results
+Console.WriteLine($"{areaOfCircle:f2}");
+Console.WriteLine($"{perimeterOfCircle:f2}");
