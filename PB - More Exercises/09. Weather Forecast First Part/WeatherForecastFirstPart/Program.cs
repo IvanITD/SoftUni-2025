ae6be7bafc775e6a90e9da708ee553205868c0a5
Ivan Ivanov <ivan78608@gmail.com>
2025-01-11 00:01:09 +0200

    Weather Forecast First and Second Part Solved

---
+++ PB - More Exercises/09. Weather Forecast First Part/WeatherForecastFirstPart/Program.cs
@@ -0,0 +1,14 @@
+﻿// User Input
+string weather = Console.ReadLine();
+
+// Check whether it's sunny or not
+if (weather == "sunny")
+{
+    // Print the output
+    Console.WriteLine("It's warm outside!");
+}
+else
+{
+    // Print the output
+    Console.WriteLine("It's cold outside!");
+} 
---
+++ PB - More Exercises/09. Weather Forecast Second Part/WeatherForecastSecondPart/Program.cs
@@ -0,0 +1,26 @@
+﻿double celcius = double.Parse(Console.ReadLine());
+
+if (celcius >= 26.00 && celcius <= 35.00)
+{
+    Console.WriteLine("Hot");
+}
+else if (celcius >= 20.1 && celcius <= 25.9)
+{
+    Console.WriteLine("Warm");
+}
+else if (celcius >= 15.00 && celcius <= 20.00)
+{
+    Console.WriteLine("Mild");
+}
+else if (celcius >= 12.00 && celcius <= 14.9)
+{
+    Console.WriteLine("Cool");
+}
+else if (celcius >= 5.00 && celcius <= 11.9)
+{
+    Console.WriteLine("Cold");
+}
+else
+{
+    Console.WriteLine("unknown");
+}
