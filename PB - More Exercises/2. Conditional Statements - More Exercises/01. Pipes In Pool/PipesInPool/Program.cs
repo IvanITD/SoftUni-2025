93ebbcb588ae8209b3ba4039fc131fbb242d2a1e
Ivan Ivanov <ivan78608@gmail.com>
2025-01-12 11:27:32 +0200

    Started doing the Pipes in Pool

--- PB - More Exercises/2. Conditional Statements - More Exercises/01. Pipes In Pool/PipesInPool/Program.cs
+++ PB - More Exercises/2. Conditional Statements - More Exercises/01. Pipes In Pool/PipesInPool/Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿//User Inputs
+int v = int.Parse(Console.ReadLine());
+int p1 = int.Parse(Console.ReadLine());
+int p2 = int.Parse(Console.ReadLine());
+double h = double.Parse(Console.ReadLine());
+
