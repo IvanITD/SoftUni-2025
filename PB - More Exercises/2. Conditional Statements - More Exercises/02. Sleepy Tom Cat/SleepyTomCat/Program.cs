13d2ec56a892c936c141f7df570a75b4d9076501
Ivan Ivanov <ivan78608@gmail.com>
2025-01-12 15:49:00 +0200

    Made some changes and started some tasks

---
+++ PB - More Exercises/2. Conditional Statements - More Exercises/02. Sleepy Tom Cat/SleepyTomCat/Program.cs
@@ -0,0 +1,31 @@
+﻿//User Input
+int restDays = int.Parse(Console.ReadLine());
+
+//Known data
+int normPlayTime = 30000;
+int workPlayTime = 63;
+int restPlayTime = 127;
+//Working days
+double workingDays = 365 - restDays;
+
+//Actual time for play
+double actualTimeForPlay = (workingDays * workPlayTime) + (restDays * restPlayTime);
+
+//Norm difference
+double normDifference = normPlayTime - actualTimeForPlay;
+
+//Minutes to hours
+double normHours = Math.Floor(normDifference / 60);
+double normMinutes = normDifference % 60;
+
+//Checking if the time for play is more than the norm for this year
+if (normDifference > normPlayTime)
+{
+    Console.WriteLine("Tom will run away");
+    Console.WriteLine("{normHours} hours and {normMinutes} minutes more for play");
+}
+else if (normDifference < normPlayTime)
+{
+    Console.WriteLine("Tom sleeps well");
+    Console.WriteLine($"{}");
+}
