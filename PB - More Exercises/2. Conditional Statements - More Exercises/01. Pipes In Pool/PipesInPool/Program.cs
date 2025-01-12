4a759dfc8586adfd2be21be8dd8eb6d76176d359
Ivan Ivanov <ivan78608@gmail.com>
2025-01-12 12:07:49 +0200

    Solved the Pipes in Pool task from PB - More Exercises

--- PB - More Exercises/2. Conditional Statements - More Exercises/01. Pipes In Pool/PipesInPool/Program.cs
+++ PB - More Exercises/2. Conditional Statements - More Exercises/01. Pipes In Pool/PipesInPool/Program.cs
@@ -4,3 +4,26 @@ int p1 = int.Parse(Console.ReadLine());
 int p2 = int.Parse(Console.ReadLine());
 double h = double.Parse(Console.ReadLine());
 
+//For curtain amount of hours
+double p1H = p1 * h;
+double p2H = p2 * h;
+
+//Total pipe hours
+double totalPipeLiters = p1H + p2H;
+
+//Total pipe percentage
+double totalPoolPercentage = (totalPipeLiters / v) * 100;
+
+//Percentage for each pipe
+double p1Percentage = (p1H / totalPipeLiters) * 100;
+double p2Percentage = (p2H / totalPipeLiters) * 100;
+
+//Overflow
+if (totalPipeLiters > v)
+{
+    Console.WriteLine($"For {h} hours the pool overflows with {totalPipeLiters - v} liters.");
+}
+else
+{
+    Console.WriteLine($"The pool is {totalPoolPercentage:f2}% full Pipe 1: {p1Percentage:f2}%. Pipe 2: {p2Percentage:f2}%.");
+}
