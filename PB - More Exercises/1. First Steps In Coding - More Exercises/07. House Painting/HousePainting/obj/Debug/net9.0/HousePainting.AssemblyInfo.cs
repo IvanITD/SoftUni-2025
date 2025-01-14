5a589276855c62523c984d168ad71c64c60ece1b
Ivan Ivanov <ivan78608@gmail.com>
2025-01-15 01:40:28 +0200

    Solved the task Flower Shop from the PB - More Exercises

--- PB - More Exercises/1. First Steps In Coding - More Exercises/07. House Painting/HousePainting/obj/Debug/net9.0/HousePainting.AssemblyInfo.cs
+++ PB - More Exercises/1. First Steps In Coding - More Exercises/07. House Painting/HousePainting/obj/Debug/net9.0/HousePainting.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HousePainting")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e7e72d3ad2cd841af5b67fa0750463ac502fa14")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47e59efb68ff118c087f632aa72ee8443d7de0d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("HousePainting")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HousePainting")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- PB - More Exercises/2. Conditional Statements - More Exercises/06. Flower Shop/FlowerShop/Program.cs
+++ PB - More Exercises/2. Conditional Statements - More Exercises/06. Flower Shop/FlowerShop/Program.cs
@@ -1,1 +1,47 @@
-﻿
+﻿// User Inputs
+int magnolias = int.Parse(Console.ReadLine());
+int hyacinths = int.Parse(Console.ReadLine());
+int roses = int.Parse(Console.ReadLine());
+int cacti = int.Parse(Console.ReadLine());
+double giftPrice = double.Parse(Console.ReadLine());
+
+// Flower Prices
+double magnoliasPrice = 3.25;
+double hyacinthsPrice = 4.00;
+double rosesPrice = 3.50;
+double cactiPrice = 8.00;
+
+// 5% tax
+double tax = 0.05;
+
+// Each flower price multiplied by the quantity
+double magnoliasTotal = magnolias * magnoliasPrice;
+double hyacinthsTotal = hyacinths * hyacinthsPrice;
+double rosesTotal = roses * rosesPrice;
+double cactiTotal = cacti * cactiPrice;
+
+// Total Flower Price
+double totalFlowerPrice = magnoliasTotal + hyacinthsTotal + rosesTotal + cactiTotal;
+
+// Total Price after tax
+double taxedPrice = totalFlowerPrice - (totalFlowerPrice * tax);
+
+
+
+// Checking if the money is enough
+if (taxedPrice >= giftPrice)
+{
+    // Calculating to check if there's enough money
+    double moneyLeft = taxedPrice - giftPrice;
+
+    // Printing the output
+    Console.WriteLine($"She is left with {Math.Floor(moneyLeft)} leva.");
+}
+else
+{
+    // Calculating to check if there's enough money
+    double moneyNeeded = giftPrice - taxedPrice;
+
+    // Printing the output
+    Console.WriteLine($"She will have to borrow {Math.Ceiling(moneyNeeded)} leva.");
+}
