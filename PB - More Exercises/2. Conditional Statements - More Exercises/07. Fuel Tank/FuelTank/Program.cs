d76fbedb6bdb1bdb2d6fea429d15b167f04a72e5
Ivan Ivanov <ivan78608@gmail.com>
2025-01-16 23:35:30 +0200

    Fuel Tank Part 1 Solved

--- PB - More Exercises/2. Conditional Statements - More Exercises/07. Fuel Tank/FuelTank/Program.cs
+++ PB - More Exercises/2. Conditional Statements - More Exercises/07. Fuel Tank/FuelTank/Program.cs
@@ -1,1 +1,40 @@
-﻿
+﻿string fuelType = Console.ReadLine();
+double litresFuel = double.Parse(Console.ReadLine());
+
+if (fuelType == "Diesel")
+{
+    if (litresFuel >= 25)
+    {
+        Console.WriteLine($"You have enough {fuelType.ToLower()}.");
+    }
+    else if (litresFuel < 25)
+    {
+        Console.WriteLine($"Fill your tank with {fuelType.ToLower()}!");
+    }
+}
+else if (fuelType == "Gasoline")
+{
+    if (litresFuel >= 25)
+    {
+        Console.WriteLine($"You have enough {fuelType.ToLower()}.");
+    }
+    else if (litresFuel < 25)
+    {
+        Console.WriteLine($"Fill your tank with {fuelType.ToLower()}!");
+    }
+}
+else if (fuelType == "Gas")
+{
+    if (litresFuel >= 25)
+    {
+        Console.WriteLine($"You have enough {fuelType.ToLower()}.");
+    }
+    else if (litresFuel < 25)
+    {
+        Console.WriteLine($"Fill your tank with {fuelType.ToLower()}!");
+    }
+}
+else
+{
+    Console.WriteLine("Invalid fuel!");
+}
