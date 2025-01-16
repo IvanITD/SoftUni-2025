4dc827dc7bda4c133117babca9f3a57dcb766e0b
Ivan Ivanov <ivan78608@gmail.com>
2025-01-16 23:52:49 +0200

    Added the task Fuel Tank Part 2

--- PB - More Exercises/2. Conditional Statements - More Exercises/05. Pets/Pets/obj/Debug/net9.0/Pets.AssemblyInfo.cs
+++ PB - More Exercises/2. Conditional Statements - More Exercises/05. Pets/Pets/obj/Debug/net9.0/Pets.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pets")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a86a11bbb3991cf259b3d39150887b8fc706060")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93c5647d0fcb5c90c26afd748abb0aecc6a142c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pets")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pets")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Programming Basics with C#/3. Conditional Statements - Lab/04. Password Guess/PasswordGuess/obj/Debug/net8.0/PasswordGuess.AssemblyInfo.cs
+++ Programming Basics with C#/3. Conditional Statements - Lab/04. Password Guess/PasswordGuess/obj/Debug/net8.0/PasswordGuess.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PasswordGuess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a86a11bbb3991cf259b3d39150887b8fc706060")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93c5647d0fcb5c90c26afd748abb0aecc6a142c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("PasswordGuess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PasswordGuess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
