71b472d1bff34f3ec8d70d43b2934cf0be4e1c32
Ivan Ivanov <ivan78608@gmail.com>
2025-01-17 00:34:22 +0200

    Finished the Fuel Tank Part 2 Task from the PB More Exercises

--- PB - More Exercises/2. Conditional Statements - More Exercises/08. Fuel Tank - Part 2/FuelTankPart2/Program.cs
+++ PB - More Exercises/2. Conditional Statements - More Exercises/08. Fuel Tank - Part 2/FuelTankPart2/Program.cs
@@ -1,2 +1,165 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// User Input
+string fuelType = Console.ReadLine(); // Could be "Gas", "Gasoline" or "Diesel"
+double fuelQuantity = double.Parse(Console.ReadLine()); // In liters
+string clubCardCheck = Console.ReadLine(); // Could be "Yes" or "No"
+
+//Fuel Prices
+double gasolinePrice = 2.22;
+double dieselPrice = 2.33;
+double gasPrice = 0.93;
+
+double clubCard = 0.0;
+double clubCardDiscount = 0.0;
+double totalPrice = 0.0;
+// Calculate the price for each fuel type after litters
+double gasolineTotal = fuelQuantity * gasolinePrice;
+double dieselTotal = fuelQuantity * dieselPrice;
+double gasTotal = fuelQuantity * gasPrice;
+
+// Check whether the Fuel is Gas, Gasoline or Diesel
+if (fuelType == "Gas")
+{
+    // Check whether the user has a club card or not
+    if (clubCardCheck == "Yes")
+    {
+        clubCard = 0.08;
+        
+        // 0.08 lv for a liter of gas
+        clubCardDiscount = fuelQuantity * clubCard;
+
+        // Now to remove the club card cents from the total price
+        gasTotal -= clubCardDiscount;
+
+        // Check whether the litres are more than 20 and 25
+        if  (fuelQuantity >= 20 && fuelQuantity <= 25)
+        {
+            // 8% discount on the total price
+            totalPrice = gasTotal - (gasTotal * 0.08);
+        }
+        else if (fuelQuantity > 25)
+        {
+            // 10% discount on the total Price
+            totalPrice = gasTotal - (gasTotal * 0.10);
+        }
+        else
+        {
+            totalPrice = gasTotal;
+        }
+    }
+    else if (clubCardCheck == "No")
+    {
+        // Check whether the litres are more than 20 and 25
+        if  (fuelQuantity >= 20 && fuelQuantity <= 25)
+        {
+            // 8% discount on the total price
+            totalPrice = gasTotal - (gasTotal * 0.08);
+        }
+        else if (fuelQuantity > 25)
+        {
+            // 10% discount on the total Price
+            totalPrice = gasTotal - (gasTotal * 0.10);
+        }
+        else
+        {
+            totalPrice = gasTotal;
+        }
+    }
+}
+else if (fuelType == "Gasoline")
+{
+    // Check whether the user has a club card or not
+    if (clubCardCheck == "Yes")
+    {
+        clubCard = 0.18;
+
+        // 0.18 lv for a liter of gasoline
+        clubCardDiscount = fuelQuantity * clubCard;
+
+        // Now to remove the club card cents from the total price
+        gasolineTotal -= clubCardDiscount;
+
+        // Check whether the litres are more than 20 and 25
+        if  (fuelQuantity >= 20 && fuelQuantity <= 25)
+        {
+            // 8% discount on the total price
+            totalPrice = gasolineTotal - (gasolineTotal * 0.08);
+        }
+        else if (fuelQuantity > 25)
+        {
+            // 10% discount on the total Price
+            totalPrice = gasolineTotal - (gasolineTotal * 0.10);
+        }
+        else
+        {
+            totalPrice = gasolineTotal;
+        }
+    }
+    else if (clubCardCheck == "No")
+    {
+        // Check whether the litres are more than 20 and 25
+        if  (fuelQuantity >= 20 && fuelQuantity <= 25)
+        {
+            // 8% discount on the total price
+            totalPrice = gasolineTotal - (gasolineTotal * 0.08);
+        }
+        else if (fuelQuantity > 25)
+        {
+            // 10% discount on the total Price
+            totalPrice = gasolineTotal - (gasolineTotal * 0.10);
+        }
+        else
+        {
+            totalPrice = gasolineTotal;
+        }
+    }
+}
+else if (fuelType == "Diesel")
+{
+    // Check whether the user has a club card or not
+    if (clubCardCheck == "Yes")
+    {
+        clubCard = 0.12;
+
+        // 0.12 lv for a liter of diesel
+        clubCardDiscount = fuelQuantity * clubCard;
+
+        // Now to remove the club card cents from the total price
+        dieselTotal -= clubCardDiscount;
+
+        // Check whether the litres are more than 20 and 25
+        if  (fuelQuantity >= 20 && fuelQuantity <= 25)
+        {
+            // 8% discount on the total price
+            totalPrice = dieselTotal - (dieselTotal * 0.08);
+        }
+        else if (fuelQuantity > 25)
+        {
+            // 10% discount on the total Price
+            totalPrice = dieselTotal - (dieselTotal * 0.10);
+        }
+        else
+        {
+            totalPrice = dieselTotal;
+        }
+    }
+    else if (clubCardCheck == "No")
+    {
+        // Check whether the litres are more than 20 and 25
+        if  (fuelQuantity >= 20 && fuelQuantity <= 25)
+        {
+            // 8% discount on the total price
+            totalPrice = dieselTotal - (dieselTotal * 0.08);
+        }
+        else if (fuelQuantity > 25)
+        {
+            // 10% discount on the total Price
+            totalPrice = dieselTotal - (dieselTotal * 0.10);
+        }
+        else
+        {
+            totalPrice = dieselTotal;
+        }
+    }
+}
+
+Console.WriteLine($"{totalPrice:f2} lv.");
--- PB - More Exercises/2. Conditional Statements - More Exercises/08. Fuel Tank - Part 2/FuelTankPart2/obj/Debug/net9.0/FuelTankPart2.AssemblyInfo.cs
+++ PB - More Exercises/2. Conditional Statements - More Exercises/08. Fuel Tank - Part 2/FuelTankPart2/obj/Debug/net9.0/FuelTankPart2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FuelTankPart2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93c5647d0fcb5c90c26afd748abb0aecc6a142c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dc827dc7bda4c133117babca9f3a57dcb766e0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("FuelTankPart2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FuelTankPart2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
