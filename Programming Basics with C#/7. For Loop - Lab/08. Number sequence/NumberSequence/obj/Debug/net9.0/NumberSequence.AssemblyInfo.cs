329c42f13e92a32c50cd03859de9f805b67b3a03
Ivan Ivanov <ivan78608@gmail.com>
2025-01-17 14:42:08 +0200

    Started new task called Left And Right Sum from the Programming Basics with C# in For Loop - Lab

--- Programming Basics with C#/7. For Loop - Lab/08. Number sequence/NumberSequence/obj/Debug/net9.0/NumberSequence.AssemblyInfo.cs
+++ Programming Basics with C#/7. For Loop - Lab/08. Number sequence/NumberSequence/obj/Debug/net9.0/NumberSequence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NumberSequence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b883e62a80d153b3295aa119167a9c4b54b852aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6f08158e017124d075fc6a182929e336554228a")]
 [assembly: System.Reflection.AssemblyProductAttribute("NumberSequence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NumberSequence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Programming Basics with C#/7. For Loop - Lab/09. Left and Right Sum/LeftAndRightSum/Program.cs
+++ Programming Basics with C#/7. For Loop - Lab/09. Left and Right Sum/LeftAndRightSum/Program.cs
@@ -1,3 +1,1 @@
-﻿int n1 = int.Parse(Console.ReadLine());
-int n2 = int.Parse(Console.ReadLine());
-
+﻿
