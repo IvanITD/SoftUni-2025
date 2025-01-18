456998984c10258ad3a196bd2aed0d87b9f1fdd8
Ivan Ivanov <ivan78608@gmail.com>
2025-01-18 18:46:01 +0200

    Bike Race from PB - More Exercises Solved

--- PB - More Exercises/3. Conditional Statements Advanced - More Exercises/02. Bike Race/BikeRace/Program.cs
+++ PB - More Exercises/3. Conditional Statements Advanced - More Exercises/02. Bike Race/BikeRace/Program.cs
@@ -1,1 +1,55 @@
-﻿
+﻿// User Inputs
+int juniors = int.Parse(Console.ReadLine());
+int seniors = int.Parse(Console.ReadLine());
+string route = Console.ReadLine();
+
+// Keep the junior's and senior's prices in variables
+double juniorPrice = 0.0;
+double seniorPrice = 0.0;
+
+// Calculate the total number of riders
+int totalRiders = juniors + seniors;
+
+// Check the route
+switch (route)
+{
+    case "trail":
+    juniorPrice = 5.50;
+    seniorPrice = 7.00;
+    break;
+
+    case "cross-country":
+    juniorPrice = 8.00;
+    seniorPrice = 9.50;
+
+    if (totalRiders >= 50)
+    {
+        juniorPrice -= juniorPrice * 0.25;
+        seniorPrice -= seniorPrice * 0.25;
+    }
+    break;
+
+    case "downhill":
+    juniorPrice = 12.25;
+    seniorPrice = 13.75;
+    break;
+
+    case "road":
+    juniorPrice = 20.00;
+    seniorPrice = 21.50;
+    break;
+
+}
+
+// Calculate the price for each type of rider
+double juniorTotalPrice = juniors * juniorPrice;
+double seniorTotalPrice = seniors * seniorPrice;
+
+// Calculate the total price
+double total = juniorTotalPrice + seniorTotalPrice;
+
+// The expenses are 5% of the total price
+double expenses = total * 0.05;
+
+Console.WriteLine($"{total - expenses:f2}");
+
--- PB - More Exercises/3. Conditional Statements Advanced - More Exercises/02. Bike Race/BikeRace/obj/Debug/net9.0/BikeRace.AssemblyInfo.cs
+++ PB - More Exercises/3. Conditional Statements Advanced - More Exercises/02. Bike Race/BikeRace/obj/Debug/net9.0/BikeRace.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BikeRace")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89d22d10ce52275b8b865b471a45fea47102b260")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a037487783200a8ad24d6166c726e42bdf37f48")]
 [assembly: System.Reflection.AssemblyProductAttribute("BikeRace")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BikeRace")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
