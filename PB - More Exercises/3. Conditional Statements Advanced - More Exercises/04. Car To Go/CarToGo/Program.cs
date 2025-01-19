2fc0f4d11229ae1c7d2e97fdd706de47f1096d77
Ivan Ivanov <ivan78608@gmail.com>
2025-01-19 15:14:45 +0200

    Car to Go Solved

--- PB - More Exercises/3. Conditional Statements Advanced - More Exercises/04. Car To Go/CarToGo/Program.cs
+++ PB - More Exercises/3. Conditional Statements Advanced - More Exercises/04. Car To Go/CarToGo/Program.cs
@@ -1,1 +1,75 @@
-﻿
+﻿// User Input
+double budget = double.Parse(Console.ReadLine());
+string season = Console.ReadLine();
+
+// variables for later use
+string classType = "";
+string carType = "";
+double carPrice = 0.0;
+
+
+// Check if budget is less than 100
+if (budget <= 100)
+{
+    // If the budget is <= 100 then the class type is Eco
+    classType = "Economy class";
+
+    // Check what the season is
+    if (season == "Summer")
+    {
+        // If the season is summer then the car type is Cabrio
+        carType = "Cabrio";
+
+        // The price is 35% from the budget
+        carPrice = budget * 0.35;
+    }
+    else if (season == "Winter")
+    {
+        // If the season is winter then the car type is Jeep
+        carType  = "Jeep";
+
+        // The price is 65% from the budget
+        carPrice = budget * 0.65;
+    }
+    
+}
+else if (budget > 100 && budget <= 500)
+{
+    // If the budget is > 100 and <= 500 then the class type is Compact
+    classType = "Compact class";
+
+    // Check what the season is
+    if (season == "Summer")
+    {
+        // If the season is summer then the car type is Cabrio
+        carType = "Cabrio";
+
+        // The price is 45% from the budget
+        carPrice = budget * 0.45;
+    }
+    else if (season == "Winter")
+    {
+        // If the season is winter then the car type is Jeep
+        carType = "Jeep";
+
+        // The price is 80% from the budget
+        carPrice = budget * 0.80;
+    }
+}
+else if (budget > 500)
+{
+    // If the budget is > 500 then the class type is Luxury
+    classType = "Luxury class";
+
+    if (season == "Summer" || season == "Winter")
+    {
+        // If the season is summer or winter then the car type is Jeep
+        carType = "Jeep";
+
+        // The price is 90% from the budget
+        carPrice = budget * 0.90;
+    }
+}
+
+Console.WriteLine($"{classType}");
+Console.WriteLine($"{carType} - {carPrice:f2}");
--- PB - More Exercises/3. Conditional Statements Advanced - More Exercises/04. Car To Go/CarToGo/obj/Debug/net9.0/CarToGo.AssemblyInfo.cs
+++ PB - More Exercises/3. Conditional Statements Advanced - More Exercises/04. Car To Go/CarToGo/obj/Debug/net9.0/CarToGo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarToGo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da822ea6ac944564f704d77e6147fba9e37efb82")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ba321d26a04dd2dc79af7676d60eeca81369696")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarToGo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarToGo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
