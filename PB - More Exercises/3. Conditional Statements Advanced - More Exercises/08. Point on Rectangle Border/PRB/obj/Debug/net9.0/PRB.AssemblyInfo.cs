1341d370ea1046338cd721ac186f7df700c975ed
Ivan Ivanov <ivan78608@gmail.com>
2025-01-23 01:51:48 +0200

    Solved Numbers from 1 to 10 from PB - More Exercises

--- PB - More Exercises/3. Conditional Statements Advanced - More Exercises/08. Point on Rectangle Border/PRB/obj/Debug/net9.0/PRB.AssemblyInfo.cs
+++ PB - More Exercises/3. Conditional Statements Advanced - More Exercises/08. Point on Rectangle Border/PRB/obj/Debug/net9.0/PRB.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PRB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fc2a624f8e0f51e96a2155014628003142b7088")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+562b646fb1dd86c72d9d6e19ab87e01f689a0258")]
 [assembly: System.Reflection.AssemblyProductAttribute("PRB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PRB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- PB - More Exercises/3. Conditional Statements Advanced - More Exercises/09. Numbers from 1 to 10/NumbersFrom1To10/Program.cs
+++ PB - More Exercises/3. Conditional Statements Advanced - More Exercises/09. Numbers from 1 to 10/NumbersFrom1To10/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿// Print numbers from 1 to 10 using a while loop.
+while (true)
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        Console.WriteLine(i);
+    }
+    break;
+}
