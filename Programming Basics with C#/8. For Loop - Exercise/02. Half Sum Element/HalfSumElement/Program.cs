9ba3deede2bded739e815936db3817b505cc836a
Ivan Ivanov <ivan78608@gmail.com>
2025-01-26 18:01:48 +0200

    Solved task "Half Sum Element" from Programming Basics with C#

--- Programming Basics with C#/8. For Loop - Exercise/02. Half Sum Element/HalfSumElement/Program.cs
+++ Programming Basics with C#/8. For Loop - Exercise/02. Half Sum Element/HalfSumElement/Program.cs
@@ -1,1 +1,39 @@
-﻿
+﻿// User input
+int n = int.Parse(Console.ReadLine());
+
+// Creating a variable named Sum for later use
+int sum = 0;
+
+// Creating a variable for Max Number
+int maxNum = int.MinValue;
+
+// Creating a for loop to cyrcle through the number n times
+for (int i = 0; i < n; i++)
+{
+  // User inputs n times a number
+  int num = int.Parse(Console.ReadLine());
+  sum += num;
+
+  // Checking to see if the number is bigger than the max number
+  if (num > maxNum)
+  {
+    maxNum = num;
+  }
+}
+
+// Subtracting the max number from the sum
+int sumWithoutMaxNumber = sum - maxNum;
+
+// Checking to see if the sum is equal to the max number
+if (maxNum == sumWithoutMaxNumber)
+{
+  // Printing the result to the User
+  Console.WriteLine("Yes");
+  Console.WriteLine($"Sum = {maxNum}");
+}
+else
+{
+  // Printing the result to the User
+  Console.WriteLine("No");
+  Console.WriteLine($"Diff = {Math.Abs(maxNum - sumWithoutMaxNumber)}");
+}
