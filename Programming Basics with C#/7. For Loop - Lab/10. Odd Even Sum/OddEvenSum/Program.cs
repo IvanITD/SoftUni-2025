849d512f3a0b532986f333a1d5c6b7a529e46f44
Ivan Ivanov <ivan78608@gmail.com>
2025-01-26 13:53:26 +0200

    Task Odd Even Sum Solved, from Programming Basics with C#

--- Programming Basics with C#/7. For Loop - Lab/10. Odd Even Sum/OddEvenSum/Program.cs
+++ Programming Basics with C#/7. For Loop - Lab/10. Odd Even Sum/OddEvenSum/Program.cs
@@ -1,1 +1,36 @@
-﻿
+﻿// User input
+int n = int.Parse(Console.ReadLine());
+
+// Create variables to store the even and odd sums
+int evenSum = 0;
+int oddSum = 0;
+
+// Loop through the number from 1 to number
+for (int i = 1; i <= n; i++)
+{
+  // Check if the current number is even or odd
+  if (i % 2 == 0)
+  {
+    // Adding the number to the even sum
+    evenSum += int.Parse(Console.ReadLine());
+  }
+  else
+  {
+    // Adding the number to the odd sum
+    oddSum += int.Parse(Console.ReadLine());
+  }
+}
+
+// Check if the sums are equal
+if (evenSum == oddSum)
+{
+  // Printing the result to the user
+  Console.WriteLine("Yes");
+  Console.WriteLine($"Sum = {evenSum}");
+}
+else
+{
+  // Printing the result to the user
+  Console.WriteLine("No");
+  Console.WriteLine($"Diff = {Math.Abs(evenSum - oddSum)}");
+}
