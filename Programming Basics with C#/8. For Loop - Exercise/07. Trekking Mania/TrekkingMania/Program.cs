ab67b87b16ecabbfff45dae2b05ae286f4b8cfbc
Ivan Ivanov <ivan78608@gmail.com>
2025-01-29 23:07:41 +0200

    Still have to finish the task

--- Programming Basics with C#/8. For Loop - Exercise/07. Trekking Mania/TrekkingMania/Program.cs
+++ Programming Basics with C#/8. For Loop - Exercise/07. Trekking Mania/TrekkingMania/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿// User Input
+int groups = int.Parse(Console.ReadLine());
+
+// Based on the groups we will create them using for loop
+for (int i = 0; i < groups; i++)
+{
+    // User Input
+    int people = int.Parse(Console.ReadLine());
+
+    // Based on the people we will create them using for loop
+    for (int j = 0; j < people; j++)
+    {
+        // User Input
+        double km = double.Parse(Console.ReadLine());
+    }
+}
