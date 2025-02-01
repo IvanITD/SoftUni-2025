d7fcb3ae569cca17db71b7bd5bcd9741af309640
Ivan Ivanov <ivan78608@gmail.com>
2025-02-01 19:34:05 +0200

    Solved another task named "Account Balance" from the Programming Basics with C#

--- Programming Basics with C#/9. While Loop - Lab/05. Account Balance/AccountBalance/obj/Debug/net9.0/AccountBalance.AssemblyInfo.cs
+++ Programming Basics with C#/9. While Loop - Lab/05. Account Balance/AccountBalance/obj/Debug/net9.0/AccountBalance.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AccountBalance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e6ca6efc26ec0cacc9013fbba6e5b02a885b585")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd5451143b36962a4acb06b28a0a766952190822")]
 [assembly: System.Reflection.AssemblyProductAttribute("AccountBalance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AccountBalance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
