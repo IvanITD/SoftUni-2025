e27b32bdd05c88cc500f6b172d468d6dc9fd1fce
Ivan Ivanov <ivan78608@gmail.com>
2025-02-05 00:41:50 +0200

    Unfinished Task

--- Programming Basics with C#/10. While Loop - Exercise/01. Old Books/OldBooks/Program.cs
+++ Programming Basics with C#/10. While Loop - Exercise/01. Old Books/OldBooks/Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿// User Input
+string favoriteBook = Console.ReadLine();
+
+// Counter for books
+int bookCounter = 0;
+
+// While Loop
+while (true)
+{
+    // User Checks Books
+    string book = Console.ReadLine();
+    
+    // Book counter
+    bookCounter++;
+
+
+    // If the book is found
+    if (book == favoriteBook)
+    {
+        Console.WriteLine($"You checked {bookCounter} books and found it.");
+        break;
+    }
+    else if (book == "No More Books")
+    {
+        Console.WriteLine("The book you search is not here!");
+        Console.WriteLine($"You checked {bookCounter} books.");
+        break;
+    }   
+}
--- Programming Basics with C#/6. Conditional Statements Advanced - Exercise/05. Journey/Journey/obj/Debug/net9.0/Journey.AssemblyInfo.cs
+++ Programming Basics with C#/6. Conditional Statements Advanced - Exercise/05. Journey/Journey/obj/Debug/net9.0/Journey.AssemblyInfo.cs
@@ -13,7 +13,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Journey")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< HEAD
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89dfd466ae6eae1ca1f948168b2d6dbe4d1dcae6")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d15d1d88d2c74fa19b09aa766b71794478d3ec1")]
+>>>>>>> 7a3c1da1 (Unfinished Task)
 [assembly: System.Reflection.AssemblyProductAttribute("Journey")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Journey")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
