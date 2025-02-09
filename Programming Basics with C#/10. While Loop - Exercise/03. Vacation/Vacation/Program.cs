47a82e077e2e0f31f29061ca61bdfeea68bb1af4
Ivan Ivanov <ivan78608@gmail.com>
2025-02-09 13:53:21 +0200

    Solved the task named "Vacation" from the Programming Basics with C#

--- Programming Basics with C#/10. While Loop - Exercise/03. Vacation/Vacation/Program.cs
+++ Programming Basics with C#/10. While Loop - Exercise/03. Vacation/Vacation/Program.cs
@@ -1,1 +1,39 @@
-﻿
+﻿// User Input
+double neededMoney = double.Parse(Console.ReadLine());
+double availableMoney = double.Parse(Console.ReadLine());
+
+int daysCount = 0;
+int spendCount = 0;
+
+while (availableMoney < neededMoney && spendCount < 5)
+{
+    string action = Console.ReadLine();
+    double amount = double.Parse(Console.ReadLine());
+
+    daysCount++;
+
+    if (action == "spend")
+    {
+        availableMoney -= amount;
+        if (availableMoney < 0)
+        {
+            availableMoney = 0;
+        }
+        spendCount++;
+    }
+    else if (action == "save")
+    {
+        availableMoney += amount;
+        spendCount = 0;
+    }
+}
+
+if (spendCount == 5)
+{
+    Console.WriteLine("You can't save the money.");
+    Console.WriteLine(daysCount);
+}
+else
+{
+    Console.WriteLine($"You saved the money for {daysCount} days.");
+}
