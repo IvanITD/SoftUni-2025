6a3fd95ef2adc32077d9ee1ff936b608d374a80c
Ivan Ivanov <ivan78608@gmail.com>
2025-02-15 10:41:12 +0200

    Added a comment section in the "Clock" task

--- Programming Basics with C#/11. Nested Loops - Lab/01. Clock/Clock/Program.cs
+++ Programming Basics with C#/11. Nested Loops - Lab/01. Clock/Clock/Program.cs
@@ -1,7 +1,9 @@
-﻿for (int h = 0; h <= 23; h++)
+﻿// Creating a for loop that prints all the hours and minutes of the day.
+for (int h = 0; h <= 23; h++)
 {
     for (int m = 0; m <= 59; m++)
     {
+        // Printing the result in the format HH:MM
         Console.WriteLine($"{h}:{m}");
     }
 }
