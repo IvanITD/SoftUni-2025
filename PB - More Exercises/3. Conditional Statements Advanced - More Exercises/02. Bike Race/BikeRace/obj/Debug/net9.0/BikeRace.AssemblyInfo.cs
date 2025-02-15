6c27dcc7a3ae35483b4efdc1fa80d3d7575b5f76
Ivan Ivanov <ivan78608@gmail.com>
2025-02-15 23:07:40 +0200

    Added all of the Console Programs to the Solution via a powershell Script

--- PB - More Exercises/3. Conditional Statements Advanced - More Exercises/02. Bike Race/BikeRace/obj/Debug/net9.0/BikeRace.AssemblyInfo.cs
+++ PB - More Exercises/3. Conditional Statements Advanced - More Exercises/02. Bike Race/BikeRace/obj/Debug/net9.0/BikeRace.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BikeRace")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+891e22fa1ecf2a604381cf569040d0d804ce4bf0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4b5ac7004b6c47fcdc22dc0438d60ee80ec02bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("BikeRace")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BikeRace")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Programming Basics with C#/2. First Steps In Coding - Exercise/02. Radians to Degrees/RadiansToDegrees/obj/Debug/net8.0/RadiansToDegrees.AssemblyInfo.cs
+++ Programming Basics with C#/2. First Steps In Coding - Exercise/02. Radians to Degrees/RadiansToDegrees/obj/Debug/net8.0/RadiansToDegrees.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RadiansToDegrees")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+891e22fa1ecf2a604381cf569040d0d804ce4bf0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4b5ac7004b6c47fcdc22dc0438d60ee80ec02bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("RadiansToDegrees")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RadiansToDegrees")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
