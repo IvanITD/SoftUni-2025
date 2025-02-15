e020f63202dfc3124d1a6ba11b8056ba84a2f3fe
Ivan Ivanov <ivan78608@gmail.com>
2025-02-15 10:45:15 +0200

    Solved the task "Multiplication Table", from the Programming Basics with C#

--- Programming Basics with C#/11. Nested Loops - Lab/02. Multiplication Table/MultiplicationTable/Program.cs
+++ Programming Basics with C#/11. Nested Loops - Lab/02. Multiplication Table/MultiplicationTable/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿// Creating a for loop that prints all the number for x and y in the range of 1 to 10.
+for (int x = 1; x <= 10; x++)
+{
+    for (int y = 1; y <= 10; y++)
+    {
+        // Printing the result in the format x * y = result
+        Console.WriteLine($"{x} * {y} = {x * y}");
+    }
+}
