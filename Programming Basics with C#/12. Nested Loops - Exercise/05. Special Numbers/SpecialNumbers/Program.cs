cd2e42c45d511bc78f59d53b8779814fcd2c1e9e
Ivan Ivanov <ivan78608@gmail.com>
2025-02-18 02:51:06 +0200

    Solved the task named "Special Numbers" from the Nested Loops - Exercise section, of the Programming Basics with C#

--- Programming Basics with C#/12. Nested Loops - Exercise/05. Special Numbers/SpecialNumbers/Program.cs
+++ Programming Basics with C#/12. Nested Loops - Exercise/05. Special Numbers/SpecialNumbers/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿int n = int.Parse(Console.ReadLine());
+
+for (int number = 1111; number <= 9999; number++)
+{
+    bool isSpecial = true;
+
+    int currentNumber = number;
+
+    while (currentNumber > 0)
+    {
+        int digit = currentNumber % 10; // Get the last digit
+
+        if (digit == 0 || n % digit != 0)
+        {
+            isSpecial = false;
+            break; // Exit loop early if any digit doesn't divide N
+        }
+
+        currentNumber /= 10; // Remove the last digit
+    }
+
+    if (isSpecial)
+    {
+        Console.Write(number + " ");
+    }
+}
--- Programming Basics with C#/12. Nested Loops - Exercise/05. Special Numbers/SpecialNumbers/obj/Debug/net9.0/SpecialNumbers.AssemblyInfo.cs
+++ Programming Basics with C#/12. Nested Loops - Exercise/05. Special Numbers/SpecialNumbers/obj/Debug/net9.0/SpecialNumbers.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SpecialNumbers")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63118db381d9728c5f03f7a90001d3def7758363")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e5c9a1b587c72fa8fa1bfb2aed4cbc7ee2c164b")]
 [assembly: System.Reflection.AssemblyProductAttribute("SpecialNumbers")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SpecialNumbers")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
