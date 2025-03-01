39b866e6fe27356e7956f44a4a55e9f179da1bdd
Ivan Ivanov <ivan78608@gmail.com>
2025-03-01 11:45:12 +0200

    Solved the task named "Football League" from the "PB - More Exercises"

--- PB - More Exercises/4. For-Loop - More Exercises/07. Football League/FootballLeague/Program.cs
+++ PB - More Exercises/4. For-Loop - More Exercises/07. Football League/FootballLeague/Program.cs
@@ -1,1 +1,47 @@
-﻿
+﻿// Read input
+int stadiumCapacity = int.Parse(Console.ReadLine());
+int totalFans = int.Parse(Console.ReadLine());
+
+// Initialize sector counters
+int sectorA = 0;
+int sectorB = 0;
+int sectorV = 0;
+int sectorG = 0;
+
+// Count fans in each sector
+for (int i = 0; i < totalFans; i++)
+{
+  string sector = Console.ReadLine();
+
+  switch (sector)
+  {
+    case "A":
+      sectorA++;
+      break;
+    case "B":
+      sectorB++;
+      break;
+    case "V":
+      sectorV++;
+      break;
+    case "G":
+      sectorG++;
+      break;
+      default:
+      break;
+  }
+}
+
+// Calculate percentages
+double sectorAPercentage = (sectorA * 100.0) / totalFans;
+double sectorBPercentage = (sectorB * 100.0) / totalFans;
+double sectorVPercentage = (sectorV * 100.0) / totalFans;
+double sectorGPercentage = (sectorG * 100.0) / totalFans;
+double totalFansPercentage = (totalFans * 100.0) / stadiumCapacity;
+
+// Print results in the correct order
+Console.WriteLine($"{sectorAPercentage:f2}%");  // First sector A
+Console.WriteLine($"{sectorBPercentage:f2}%");  // Then sector B
+Console.WriteLine($"{sectorVPercentage:f2}%");  // Then sector V
+Console.WriteLine($"{sectorGPercentage:f2}%");  // Then sector G
+Console.WriteLine($"{totalFansPercentage:f2}%"); // Finally total percentage
--- Programming Basics with C#/4. Conditional Statements - Exercise/08. Lunch Break/LunchBreak/obj/Debug/net9.0/LunchBreak.AssemblyInfo.cs
+++ Programming Basics with C#/4. Conditional Statements - Exercise/08. Lunch Break/LunchBreak/obj/Debug/net9.0/LunchBreak.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LunchBreak")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c00f07c8e46e2f8c9d4beb13b8645d31ef1f372a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b9366ea1d8b213fe7df6f38f859e048efd5accd")]
 [assembly: System.Reflection.AssemblyProductAttribute("LunchBreak")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LunchBreak")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
